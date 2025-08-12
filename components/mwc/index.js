b5e9e85f7cc84d8fe9152ee741a3242a90ce32bb
JordanGuevara <axndr154@gmail.com>
2025-08-12 12:53:26 -0500

    Primer commit

---
+++ components/mwc/index.js
@@ -0,0 +1,6 @@
+// Importar Material Web Components desde unpkg
+import 'https://unpkg.com/@material/mwc-top-app-bar@latest/mwc-top-app-bar.js?module';
+import 'https://unpkg.com/@material/mwc-card@latest/mwc-card.js?module';
+import 'https://unpkg.com/@material/mwc-button@latest/mwc-button.js?module';
+import 'https://unpkg.com/@material/mwc-icon@latest/mwc-icon.js?module';
+import 'https://unpkg.com/@material/mwc-textfield@latest/mwc-textfield.js?module';
